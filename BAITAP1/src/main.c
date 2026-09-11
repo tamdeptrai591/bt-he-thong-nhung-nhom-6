8cb74fb710904e34a62ba037fda783466d626629
huychill6789 <huyvuhuu7@gmail.com>
2026-09-11 16:40:49 +0700

    main.c

--- BAITAP1/src/main.c
+++ BAITAP1/src/main.c
@@ -1,4 +1,10 @@
-#include "stm32f103.h"
+#include <stdint.h>
+
+
+#define RCC_APB2ENR (*(volatile uint32_t *)0x40021018)
+
+#define GPIOC_CRH   (*(volatile uint32_t *)0x40011004)
+#define GPIOC_ODR   (*(volatile uint32_t *)0x4001100C)
 
 
 static void delay_ms(uint32_t ms)
@@ -7,27 +13,34 @@ static void delay_ms(uint32_t ms)
     {
         for (volatile uint32_t j = 0; j < 8000; j++)
         {
-            __NOP();
+            __asm volatile ("nop");
         }
     }
 }
 
+
 int main(void)
 {
-
-    RCC_APB2ENR |= RCC_IOPCEN;
-
+    /* 1. Bật clock cho GPIOC */
+    RCC_APB2ENR |= (1U << 4);
     GPIOC_CRH &= ~(0xFU << 20);
     GPIOC_CRH |= (0x2U << 20);
-    GPIOC_ODR |= (1U << 1);
+
+
+    /* 3. PC13 = HIGH */
+    GPIOC_ODR |= (1U << 13);
+
+
     while (1)
     {
-   
+
         GPIOC_ODR &= ~(1U << 13);
 
-        delay_ms(1000);
+        delay_ms(100);
 
+    
         GPIOC_ODR |= (1U << 13);
-        delay_ms(1000);
+
+        delay_ms(100);
     }
 }
